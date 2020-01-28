04d278520cb9b1e9a7e4c592f03d63fbe2de482a
Rafael Sá Menezes <rafael.sa@icomp.ufam.edu.br>
2020-01-28 09:29:45 -0400

    added constructor tests

--- unit/util/lru_cache.test.cpp
+++ unit/util/lru_cache.test.cpp
@@ -23,7 +23,7 @@ namespace utf = boost::unit_test;
 // Check whether the object is initialized correctly
 
 BOOST_AUTO_TEST_SUITE(constructors)
-BOOST_AUTO_TEST_CASE(ok)
+BOOST_AUTO_TEST_CASE(max_size_ok)
 {
   lru_cache<int, int> obj(10);
   size_t expected = 10;
@@ -31,11 +31,36 @@ BOOST_AUTO_TEST_CASE(ok)
   BOOST_TEST(expected == actual);
 }
 
-BOOST_AUTO_TEST_CASE(fail)
+BOOST_AUTO_TEST_CASE(max_size_fail)
 {
   lru_cache<int, int> obj(10);
   size_t expected = 11;
   size_t actual = obj.max_size();
   BOOST_TEST(expected != actual);
 }
+
+BOOST_AUTO_TEST_CASE(initial_size_ok)
+{
+  lru_cache<int, int> obj(10);
+  size_t expected = 0;
+  size_t actual = obj.size();
+  BOOST_TEST(expected == actual);
+}
+
+BOOST_AUTO_TEST_CASE(element_doest_not_exist_ok)
+{
+  lru_cache<int, int> obj(10);
+  bool expected = false;
+  bool actual = obj.exists(0);
+  BOOST_TEST(expected == actual);
+}
+
+BOOST_AUTO_TEST_CASE(get_should_return_null_ok)
+{
+  lru_cache<int, int> obj(10);
+  int *expected = NULL;
+  int *actual = obj.get(0);
+  BOOST_TEST(expected == actual);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
