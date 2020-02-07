1938727279aa08fd43c460a91e1897c1c1567c0b
Rafael Sá Menezes <rafael.sa@icomp.ufam.edu.br>
2020-02-07 11:09:23 -0400

    added bool_type2t serialization test
    Signed-off-by: Rafael Sá Menezes <rafael.sa@icomp.ufam.edu.br>

--- unit/util/irep_serialization.test.cpp
+++ unit/util/irep_serialization.test.cpp
@@ -13,6 +13,8 @@
 #define BOOST_TEST_MODULE "irep Serialization"
 
 #include <util/irep_serialization.h>
+#include <util/irep2.h>
+#include <util/irep2_type.h>
 #include <boost/test/included/unit_test.hpp>
 
 // ******************** TESTS ********************
@@ -21,7 +23,7 @@
 // Check whether the object is initialized correctly
 
 BOOST_AUTO_TEST_SUITE(serializaton)
-BOOST_AUTO_TEST_CASE(write_long_buffer)
+BOOST_AUTO_TEST_CASE(write_read_long)
 {
   unsigned expected = 1234;
   unsigned actual;
@@ -34,7 +36,7 @@ BOOST_AUTO_TEST_CASE(write_long_buffer)
   BOOST_CHECK_EQUAL(expected, actual);
 }
 
-BOOST_AUTO_TEST_CASE(write_string_buffer)
+BOOST_AUTO_TEST_CASE(write_read_string)
 {
   std::string expected = "1234";
   std::string actual;
@@ -44,6 +46,31 @@ BOOST_AUTO_TEST_CASE(write_string_buffer)
   instream.str(stream.str());
   actual = irep_serializationt::read_string(instream).as_string();
   BOOST_CHECK_EQUAL(expected, actual);
+}
+
+BOOST_AUTO_TEST_CASE(write_read_bool_type2t)
+{
+  std::ofstream ofs("filename");
+
+  bool_type2t expected;
+  expected.type_id = type2t::type_ids::code_id;
+  // save data to archive
+  {
+    boost::archive::text_oarchive oa(ofs);
+    // write class instance to archive
+    oa << expected;
+    // // archive and stream closed when destructors are called
+  }
+
+  bool_type2t actual;
+  {
+    std::ifstream ifs("filename");
+    boost::archive::text_iarchive ia(ifs);
+    // read class state from archive
+    ia >> actual;
+  }
+
+  BOOST_CHECK_EQUAL(expected.type_id, actual.type_id);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
